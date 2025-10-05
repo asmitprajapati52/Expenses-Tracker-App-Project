7382301b35c71a90c8ef7567c0eb290f3b0afa86
Asmit prajapati <asmitprajapati52@gmail.com>
2025-10-05 12:20:20 +0530

    updating code

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ let amount=document.querySelector("#amount");
 let btn=document.querySelector("#btn");
 let body=document.querySelector("tbody");
 
+window.addEventListener("load", loadNotes);
+
 let  Balance=0;
 let Income=0;
 let Expenses=0;
@@ -42,6 +44,7 @@ btn.addEventListener("click",()=>{
     Balance=Income-Expenses;
 
     updateUI();
+    saveNotes();
     name.value="";
     amount.value="";
 
@@ -64,3 +67,51 @@ function updateUI(){
     balance.textContent=Balance;
 }
 
+function saveNotes(){
+    const data=[];
+    document.querySelectorAll("tbody tr").forEach(tr=>{
+        const tds=tr.querySelectorAll("td");
+        data.push({
+            name:tds[1].textContent,
+            amount:Number(tds[2].textContent),
+            entry:tds[3].textContent
+        });
+    });
+    localStorage.setItem("trackerData", JSON.stringify(data));
+}
+
+function loadNotes(){
+    const saved=JSON.parse(localStorage.getItem("trackerData"))|| [];
+    saved.forEach((item)=>{
+        let row=document.createElement("tr");
+        row.classList.add("row");
+
+        row.innerHTML=`
+        <td>${serial++}</td>
+        <td>${item.name}</td>
+        <td>${item.amount}</td>
+        <td>${item.entry} </td>
+        <td><button class="delBtn">❌</button></td>
+        `;
+        body.appendChild(row);
+
+        if(item.entry==="Income"){
+            Income+=item.amount;
+        } else {
+            Expenses+=item.amount;
+        }
+        row.querySelector(".delBtn").addEventListener("click", () => {
+    if (item.entry === "Income") {
+      Income -= item.amount;
+    } else {
+      Expenses -= item.amount;
+    }
+    Balance = Income - Expenses;
+    updateUI();
+    row.remove();
+    saveNotes();
+  });
+});
+ Balance = Income - Expenses;
+  updateUI();
+}
